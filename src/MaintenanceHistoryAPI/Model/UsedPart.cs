1b495a7e12e1c25ed5d4bd60abf8f16269ccaa3b
Gus Theunissen <gus.theunissen142@gmail.com>
2024-10-18 10:00:33 +0200

    #14 added repairpart and other functionality

--- src/MaintenanceHistoryAPI/Model/UsedPart.cs
+++ src/MaintenanceHistoryAPI/Model/UsedPart.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using Pitstop.WorkshopManagementAPI.Domain.Entities;
 
 namespace Pitstop.MaintenanceHistoryAPI.Model;
 
-public class UsedParts
+public class UsedPart
 {
+    [Key]
     public Guid Id { get; set; }
+    
+    public int UsedQuantity { get; set; } = 1;
+
+    public Guid RepairPartId { get; set; }
+    
+    public RepairPart RepairPart { get; set; }
     
     public Guid MaintenanceJobId { get; set; }
     
