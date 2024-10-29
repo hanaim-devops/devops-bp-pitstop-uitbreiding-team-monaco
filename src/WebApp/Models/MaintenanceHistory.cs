2bcf1165b1fb0ed9679a81ede4725efa6b81ea3b
Gus Theunissen <gus.theunissen142@gmail.com>
2024-10-29 12:09:11 +0100

    #14 fixes

--- src/WebApp/Models/MaintenanceHistory.cs
+++ src/WebApp/Models/MaintenanceHistory.cs
@@ -11,5 +11,6 @@ public class MaintenanceHistory
     public MaintenanceTypes MaintenanceType { get; set; }
     public bool IsCompleted { get; set; }
     public Guid MaintenanceJobId { get; set; }
+    
     public List<UsedPart> UsedParts { get; set; }
 }
