eab9a3a4513d2fcc26ec9e01d50d39bd32af8140
Kevin Slingerland <kevinslingerland@outlook.com>
2024-10-31 20:40:01 +0100

    #69 Refactor the application to handle and process its own data, this does not belong in the workshop management API

--- src/WorkshopManagementEventHandler/Program.cs
+++ src/WorkshopManagementEventHandler/Program.cs
@@ -4,26 +4,13 @@
     {
         services.UseRabbitMQMessageHandler(hostContext.Configuration);
 
-        services.AddTransient<WorkshopManagementDBContext>((svc) =>
+        services.AddTransient(_ =>
         {
             var sqlConnectionString = hostContext.Configuration.GetConnectionString("WorkshopManagementCN");
             var dbContextOptions = new DbContextOptionsBuilder<WorkshopManagementDBContext>()
                 .UseSqlServer(sqlConnectionString)
                 .Options;
             var dbContext = new WorkshopManagementDBContext(dbContextOptions);
-
-            DBInitializer.Initialize(dbContext);
-
-            return dbContext;
-        });
-
-        services.AddTransient<MaintenanceHistoryContext>((svc) =>
-        {
-            var sqlConnectionString = hostContext.Configuration.GetConnectionString("MaintenanceHistoryCN");
-            var dbContextOptions = new DbContextOptionsBuilder<MaintenanceHistoryContext>()
-                .UseSqlServer(sqlConnectionString)
-                .Options;
-            var dbContext = new MaintenanceHistoryContext(dbContextOptions);
 
             DBInitializer.Initialize(dbContext);
 
